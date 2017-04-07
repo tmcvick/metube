7c2a717f9fe078692df6b9ac0428cb8c47844d0a
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-07 16:12:37 -0400

    debugging update user

--- php/update_user.php
+++ php/update_user.php
@@ -16,7 +16,7 @@ $channel = $_REQUEST['channel'];
 $id = $_REQUEST['id'];
 
 $sql = "UPDATE user INNER JOIN user_security ON user_security.security_id = user.security_id SET
-            (user.fname='$fname', user.lname='$lname', user.username='$uname', user.email='$email', user.channel_name='$channel', user_security.password='$pword') WHERE user_id = '$id'";
+            user.fname='$fname', user.lname='$lname', user.username='$uname', user.email='$email', user.channel_name='$channel', user_security.password='$pword' WHERE user_id = '$id'";
 if($result = mysqli_query($conn, $sql)) {
     echo 'User id update: ' . $id;
 } else {
