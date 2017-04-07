f630fe1ba124ddc84c948a5f8fbbcca5dbe2ca7d
Brendan Christopher Giberson <bgibers@webapp.cs.clemson.edu>
2017-04-07 16:36:33 -0400

    added login.php

--- login.php
+++ login.php
@@ -1,17 +1,38 @@
-<!DOCTYPE html>
-<html>
-<script language="php">
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('/home/tmcvick/public_html/'));
+<?php
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('/home/bgibers/public_html/'));
 
     foreach($iterator as $item) {
         chmod($item, 0755);
 }
-</script>
+	include_once "php/include.php";
 
-  <head>
-    <title>Hello World Tim!</title>
-  </head>
-  <body>
-    <p>Hello World</p>
-  </body>
-</html>
+	$user = $_REQUEST["user"];
+	$pswrd = $_REQUEST["pass"];
+	$usrerr = "Invalid username";
+
+	$ver_user = "SELECT security_id FROM user WHERE username = '$user' ";
+	$user_result = mysqli_query($conn,$ver_user);
+		
+	if(empty($user_result)) 
+	{
+		echo $usrerr; // should empty text field and return 
+	}
+	else
+	{
+		$sec_id = mysqli_fetch_object($user_result);
+		//echo $sec_id->security_id;
+
+		$ver_pass = "SELECT password FROM user_security WHERE security_id = '$sec_id->security_id' ";
+		$pass_result = mysqli_query($conn,$ver_pass);
+		$usr_pass = mysqli_fetch_object($pass_result); 
+		
+			if($usr_pass->password == $pswrd)
+			{
+				echo 'Success!';
+			}
+			else{
+				echo 'Login failed';
+			}
+	}
+
+?>
