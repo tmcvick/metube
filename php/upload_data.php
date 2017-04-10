0fb3493c662b8a48ee1736b818afbc27b9614fd2
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-10 12:43:03 -0400

    debugging upload_data

--- php/upload_data.php
+++ php/upload_data.php
@@ -13,45 +13,51 @@ $dtype = $_REQUEST['dtype'];
 $desc = $_REQUEST['desc'];
 $title = $_REQUEST['title'];
 $taglist = $_REQUEST['taglist'];
+//todo we need to check to ensure a user is logged in... lets do this last. session shit sucks
 $u_id  = $_SESSION['glbl_user']->user_id;
 
-
-
-
-$sql = "INSERT INTO data (type,filename,description,title,user_id) VALUES ('$flname','$dtype','$desc','$title','$u_id')";
+/* Insert data */
+$sql = "INSERT INTO data (type,filename,description,title,user_id) VALUES ('$dtype', '$flname''$desc','$title','$u_id')";
 
 if($result = mysqli_query($conn, $sql)) {
     $lastdtID = mysqli_insert_id($conn);
     echo 'Data id created: ' . $lastdtID . '<br>';
 
+    //Link each tag thats in the taglist (will be separated by commas)
+    //todo make sure the front end validates that each tag in taglist is different or this will become a clusterfuck
     $tag_array = explode(',', $taglist);
-    foreach($tag_array as $tag)
-    {
-
+    foreach($tag_array as $tag) {
         $sql = "SELECT tag_id FROM tag WHERE keyword = '$tag'";
         echo $tag;
-        $result = mysqli_query($conn,$sql);
-        if($result->num_rows == 0)
-        {
-            $sql = "INSERT INTO tag (keyword) VALUES ('$tag')";
-            if($result = mysqli_query($conn, $sql))
-            {
-                $lastID = mysqli_insert_id($conn);
-                echo 'New tag created: ' . $lastID . '<br>';
-                $sql = "SELECT '$lastID' FROM tag";
-                $result = mysqli_query($conn,$sql);
+        $tgint = null;
+        if($result = mysqli_query($conn,$sql)) {
+
+            //If tag does not exist insert it
+            if ($result->num_rows == 0) {
+                $sql = "INSERT INTO tag (keyword) VALUES ('$tag')";
+                if ($result = mysqli_query($conn, $sql)) {
+                    $lastID = mysqli_insert_id($conn);
+                    echo 'New tag created: ' . $lastID . '<br>';
+                    $tgint = $lastID;
+                }
+            }
+            //the tag already exists
+            else {
+                $tgint = mysqli_fetch_object($result)->tag_id;
             }
         }
-        else
-        {
+        else {
             echo $conn->error;
         }
-        $tgint = (int)$result;
-        $sql = "INSERT INTO data_tag (data_id,tag_id) VALUES ('$lastdtID','$tgint')";
-        if($result = mysqli_query($conn, $sql))
-        {
+
+        //insert a link between the tag and the data
+        $sql = "INSERT INTO data_tag (data_id, tag_id) VALUES ('$lastdtID','$tgint')";
+        if($result = mysqli_query($conn, $sql)) {
             $lastID = mysqli_insert_id($conn);
             echo 'New tag linked: ' . $lastID . '<br>';
+        }
+        else {
+            echo $conn->error;
         }
     }
 
