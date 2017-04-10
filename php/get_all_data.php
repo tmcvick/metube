4bd7f268535316a0a6725ce67207239015ba1841
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-10 13:04:48 -0400

    debugging select

--- php/get_all_data.php
+++ php/get_all_data.php
@@ -12,7 +12,8 @@
     if($result = mysqli_query($conn, $sql)) {
         while($row = mysqli_fetch_assoc($result)) {
             echo json_encode($row);
-            $sql = "SELECT data_id, keyword FROM tag INNER JOIN data_tag on data_tag.data_id ='$row->data_id' and data_tag.tag_id=tag.tag_id;";
+            $data_id = $row['data_id'];
+            $sql = "SELECT data_id, keyword FROM tag INNER JOIN data_tag on data_tag.data_id ='$data_id' and data_tag.tag_id=tag.tag_id;";
             if($result = mysqli_query($conn, $sql)) {
                 while($row = mysqli_fetch_assoc($result)) {
                     echo json_encode($row);
