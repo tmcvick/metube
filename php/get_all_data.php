65f2c43a1e12c5e5ee8c546f5eb1b59cf504fac8
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-10 13:13:18 -0400

    making it look nice

--- php/get_all_data.php
+++ php/get_all_data.php
@@ -16,12 +16,14 @@
             $sql = "SELECT data_id, keyword FROM tag INNER JOIN data_tag on data_tag.data_id ='$data_id' and data_tag.tag_id=tag.tag_id;";
             if($resultTag = mysqli_query($conn, $sql)) {
                 while($rowTag = mysqli_fetch_assoc($resultTag)) {
+                    echo '<br>';
                     echo json_encode($rowTag);
                 }
             } else {
                 echo "Error with getting tags <br>";
                 echo $conn->error;
             }
+            echo '<br>';
         }
     } else {
         echo "Error with getting data <br>";
