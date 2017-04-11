72d3d3e6aa94dec19a8458b0ac75cb19cfade55d
bgibers <bgibers@clemson.edu>
2017-04-10 20:33:57 -0400

    Add to playlist created

---
+++ php/add_to_playlist.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: BrendanGiberson
+ * Date: 4/10/17
+ * Time: 8:24 PM
+ */
+
+include_once "include.php";
+
+$data_id  = $_REQUEST['data_id'];
+$pl_id = $_REQUEST['pl_id'];
+
+$sql = "INSERT INTO playlist_data (data_id,playlist_id) VALUES ('$data_id','$pl_id')";
+
+if($result = mysqli_query($conn, $sql)) {
+    $playlist_data_id = mysqli_insert_id($conn);
+    echo 'Data linked: ' . $playlist_data_id . '<br>';
+} else {
+    echo $conn->error;
+}
+?>
