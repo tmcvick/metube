57ef55ea0c8738a04f51e30542df74e5a56c68c8
bgibers <bgibers@clemson.edu>
2017-04-10 20:53:13 -0400

    Add favorite added

---
+++ php/add_favorite.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: BrendanGiberson
+ * Date: 4/10/17
+ * Time: 8:24 PM
+ */
+
+include_once "include.php";
+
+$user_id  = $_SESSION['glbl_user']->user_id;
+$data_id = $_REQUEST['data_id'];
+
+$sql = "INSERT INTO user_favorite (user_id,data_id) VALUES ('$user_id','$data_id')";
+
+if($result = mysqli_query($conn, $sql)) {
+    $fav_id = mysqli_insert_id($conn);
+    echo 'Favorite added: ' . $fav_id . '<br>';
+} else {
+    echo $conn->error;
+}
+?>
