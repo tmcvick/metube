ad42edb03583cac4293c82fa61c048dc123a38b9
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-13 10:14:50 -0400

    test user

---
+++ php/test_display_session.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: timmcvicker
+ * Date: 4/13/17
+ * Time: 10:12
+ */
+include_once "include.php";
+if(isset($_SESSION['glbl_user'])) {
+    echo 'user set: ' . $_SESSION['glbl_user'];
+} else {
+    echo 'User not set';
+}
