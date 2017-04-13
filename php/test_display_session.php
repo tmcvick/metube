6a13b591a239de911d4f047a68275bd51f8be058
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-13 10:18:25 -0400

    formatting changes

--- php/test_display_session.php
+++ php/test_display_session.php
@@ -7,8 +7,8 @@
  */
 include_once "include.php";
 if(isset($_SESSION['glbl_user'])) {
-    echo "user set: ";
-    echo  $_SESSION['glbl_user'];
+    echo "user set: " . print_r($_SESSION, TRUE);
+
 } else {
     echo 'User not set';
 }
