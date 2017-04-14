66a7ba4c6e278d4e4e2e5f9a3199d64e5170a2ef
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-13 23:49:30 -0400

    tsting input

--- php/test_display_session.php
+++ php/test_display_session.php
@@ -12,3 +12,27 @@ if(isset($_SESSION['glbl_user'])) {
 } else {
     echo 'User not set';
 }
+echo '
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Media Upload</title>
+</head>
+
+<body>
+
+<form method="post" action="" enctype="multipart/form-data" >
+
+    <p style="margin:0; padding:0">
+        <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+        Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
+        <input  name="file" type="file" size="50" />
+
+        <input value="Upload" name="submit" type="submit" />
+    </p>
+
+
+</form>
+
+</body>
+</html>
+';
