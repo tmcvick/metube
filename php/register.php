c99abe553b241a256cf3e1b13ea5fe12b151f24e
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-17 15:08:50 -0400

    registration page

--- php/register.php
+++ php/register.php
@@ -12,21 +12,27 @@ $uname = $_REQUEST['uname'];
 $email = $_REQUEST['email'];
 $pword = $_REQUEST['pword'];
 $channel = $_REQUEST['channel'];
+$pword2 = $_REQUEST['pword2'];
+
+if ($pword != $pword2) {
+    echo "Error: passwords didn't match!";
+} else {
 
 
- //create secuirty table
-$sql = "INSERT INTO user_security (password) VALUES ('$pword')";
+    //create secuirty table
+    $sql = "INSERT INTO user_security (password) VALUES ('$pword')";
 
-if($result = mysqli_query($conn, $sql)) {
-    $security_id = mysqli_insert_id($conn);
-    $sql = "INSERT INTO user (fname, lname, username, email, security_id, channel_name) VALUES ('$fname', '$lname', '$uname', '$email', '$security_id', '$channel')";
+    if ($result = mysqli_query($conn, $sql)) {
+        $security_id = mysqli_insert_id($conn);
+        $sql = "INSERT INTO user (fname, lname, username, email, security_id, channel_name) VALUES ('$fname', '$lname', '$uname', '$email', '$security_id', '$channel')";
 
-    if($result = mysqli_query($conn, $sql)) {
-        echo 'User id created: ' . mysqli_insert_id($conn);
+        if ($result = mysqli_query($conn, $sql)) {
+            echo 'User id created: ' . mysqli_insert_id($conn);
+        } else {
+            echo $conn->error;
+        }
     } else {
         echo $conn->error;
     }
-} else {
-    echo $conn->error;
 }
 ?>
