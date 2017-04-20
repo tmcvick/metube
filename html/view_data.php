ee59bf4409bcb713561bfa24b849ef4ec1533a36
Olivia Guzzardo <oguzzar@clemson.edu>
2017-04-20 01:29:31 -0400

    reformatting description on media

--- html/view_data.php
+++ html/view_data.php
@@ -60,7 +60,7 @@ if ($resultData = mysqli_query($conn, $sql))
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
-<script src="js/webflow.js" type="text/javascript"></script>
+<script src="../js/webflow.js" type="text/javascript"></script>
 
 <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 
