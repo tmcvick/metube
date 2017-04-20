265b427ebc4595ed1dc4086736fde96b99ed0e99
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-20 04:25:06 -0400

    view video on website

--- html/view_data.php
+++ html/view_data.php
@@ -40,6 +40,21 @@ if ($resultData = mysqli_query($conn, $sql))
                 </div>
     <h4>' . $description . '</h4>
     </div>';
+
+    if($rowData['type'] == "video") {
+       echo '<object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
+
+<param name="filename" value="/~tmcvick/uploads/' . $rowData['filename'] . '">
+	<!-- echo $result_row[2].$result_row[1];  -->
+		
+
+<param name="Showcontrols" value="True">
+<param name="autoStart" value="True">
+
+<embed type="application/x-mplayer2" src="/~tmcvick/uploads/' . $rowData['filename'] . '" name="MediaPlayer" width=320 height=240></embed>
+
+</object> ';
+    }
 }
 
 ?>
