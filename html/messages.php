17341a14b15caf6ee1d28d9ebe3afe1575459830
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-20 01:44:28 -0400

    sql changes

--- html/messages.php
+++ html/messages.php
@@ -26,7 +26,7 @@ include "header.php";
     $user = $_SESSION['glbl_user']->user_id;
 
     $sql = "SELECT Message.*, toUser.username as toUser, fromUser.username as fromUser FROM Message INNER JOIN user fromUser on Message.`from` = fromUser.user_id INNER JOIN user toUser on Message.`created_by` = toUser.user_id
-WHERE Message.`created_by`=$user or Message.from=$user GROUP BY toUser.username ORDER BY Message.read_ind,  Message.timestamp DESC";
+WHERE Message.`created_by`=$user or Message.from=$user GROUP BY toUser.username, fromUser.username ORDER BY Message.read_ind,  Message.timestamp DESC";
 
     if ($result = mysqli_query($conn, $sql)) {
         if ($result->num_rows == 0) {
