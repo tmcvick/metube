fdd512a679d57b94d721c43a1731a693875d44a4
Olivia Guzzardo <oguzzar@clemson.edu>
2017-04-19 23:05:28 -0400

    prevent playlist from calling display_row

--- html/list_playlists.php
+++ html/list_playlists.php
@@ -45,7 +45,9 @@ if (!isset($_SESSION['glbl_user']) || empty($_SESSION['glbl_user'])) {
             if ($resultData = mysqli_query($conn, $sql)) {
                 while ($rowData = mysqli_fetch_assoc($resultData)) {
                     $data_id = $rowData['data_id'];
-                    displayRow($rowData);
+                    //displayRow($rowData);
+                    // TODO
+                    echo json_encode($rowData);
                     $sql = "SELECT data_id, keyword FROM tag INNER JOIN data_tag on data_tag.data_id ='$data_id' and data_tag.tag_id=tag.tag_id;";
                     if ($resultTag = mysqli_query($conn, $sql)) {
                         while ($rowTag = mysqli_fetch_assoc($resultTag)) {
