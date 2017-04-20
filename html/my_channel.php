6d25dc560e165ff0577786c65efa002bbb2ed376
Olivia Guzzardo <oguzzar@clemson.edu>
2017-04-20 01:53:37 -0400

    fixing media layout

--- html/my_channel.php
+++ html/my_channel.php
@@ -33,7 +33,7 @@ if (!isset($_SESSION['glbl_user']) || empty($_SESSION['glbl_user'])) {
             $sql = "SELECT data_id, keyword FROM tag INNER JOIN data_tag on data_tag.data_id ='$data_id' and data_tag.tag_id=tag.tag_id;";
             if ($resultTag = mysqli_query($conn, $sql)) {
                 if($resultTag->num_rows != 0) {
-                    echo '<div class="w-container" style="padding:10px"><strong>Keywords: &nbsp</strong>';
+                    echo '<div class="w-container" style="padding:10px" align="center"><strong>Keywords: &nbsp</strong>';
                     while ($rowTag = mysqli_fetch_assoc($resultTag)) {
                         displayRow($rowTag);
                     }
