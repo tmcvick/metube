823a451edf7d3380012834c45380d2b26e609be9
Olivia Guzzardo <oguzzar@clemson.edu>
2017-04-19 22:34:41 -0400

    changed formatting

--- php/include.php
+++ php/include.php
@@ -30,8 +30,26 @@ function displayRow($sql)
     else
     {
         //this is a data object
+        $title = $sql['title'];
+        $description = $sql['description'];
         echo '<br>';
-        echo json_encode($sql);
+        /*echo json_encode($sql);*/
+       echo '<div class="w-container">
+    <div class="form-wrapper w-clearfix w-form">
+        <form class="form-2" data-name="Display Data Form" id="wf-form-Display-Data-Form"
+              name="wf-form-Display-Data-Form" method="post">
+            <div class="w-row">
+                <div class="column-9 w-col w-col-6">
+                    <label class="lastnamelbl" for="title"><?php echo $title; ?></label>
+                </div>
+                <div class="column-10 w-col w-col-6">
+                    <label class="lastnamelbl" for="lname"><?php echo $description; ?></label>
+                </div>
+            </div>
+            </div>
+        </form>
+    </div>
+</div>';
     }
     return;
 }
