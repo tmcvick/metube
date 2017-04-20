d11c526e7e322ec8aac6e911e061dc188877430e
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-20 14:57:39 -0400

    guest work

--- html/header.php
+++ html/header.php
@@ -63,8 +63,8 @@ if (!isset($GLOBALS["guest"])) {
     <a class="w-nav-link" href="upload.php" id="uploadLink"><strong>Upload Media</strong></a>
     <a class="w-nav-link" href="messages.php" id="messagesLink"><strong>Messages</strong></a>';
      } else {
-            echo '<nav class="w-nav-menu" role="navigation">
-        <a class="nav-link w-nav-link" href="registration.html"><strong id="logoutLink">Register an Account</strong></a>
+            echo '<div class="navbar w-nav" data-animation = "default" data-collapse = "medium" data-duration = "400" > <nav class="w-nav-menu" role="navigation">
+        <a class="nav-link w-nav-link" href="login.php"><strong id="logoutLink">Back to Login Page</strong></a>
     </nav>';
 }
 ?>
