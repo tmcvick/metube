6c72057231269142fd5cd218d1b13d3e8c8e47ac
Tim McVicker <tmcvick@g.clemson.edu>
2017-04-20 02:30:23 -0400

    starting upload data

--- html/upload.php
+++ html/upload.php
@@ -20,12 +20,14 @@ include "header.php";
         <input class="text-field-8 w-input" data-name="titleTxt" id="title" maxlength="256" name="title" placeholder="Enter the title of the media" type="text" required>
         <label for="typeText" id="typeLbl">Type</label>
         <input class="text-field-9 w-input" data-name="typeText" id="dtype" maxlength="256" name="dtype" placeholder="Enter the type of media (i.e. picture, video)" required="required" type="text">
-        <label for="typefname" id="typeLbl">Filename</label>
-        <input class="text-field-9 w-input" data-name="typefname" id="flname" maxlength="256" name="flname" placeholder="Enter the filename" required="required" type="text">
         <label for="taglist" id="typeLbl">Tags</label>
         <input class="text-field-9 w-input" data-name="taglist" id="taglist" maxlength="1000" name="taglist" placeholder="Enter the keywords, separated by commas" required="required" type="text">
         <label for="descriptionTxt" id="descriptionLbl">Description</label>
         <textarea class="textarea w-input" id="desc" maxlength="5000" name="desc" required placeholder="Add a short description of the media"></textarea>
+        <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+        Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
+        <input required name="flname" id="flname" type="file" size="50" />
+
         <input class="submit-button-4 w-button" data-wait="Please wait..." id="uploadBtn" type="submit" value="Upload">
     </form>
 </div>
