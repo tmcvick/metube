6e5a6c9b4b97fe83a6e9a3cd7877b2a6f1ea2406
Olivia Guzzardo <oguzzar@clemson.edu>
2017-04-20 15:11:41 -0400

    editing view data format

--- html/view_data.php
+++ html/view_data.php
@@ -64,7 +64,7 @@ This audio failed to play
 ?>
 <div class="container-11 w-container">
     <div class="w-row">
-        <div class="column-18 w-col w-col-6"><a class="button-14 w-button"
+        <div class="column-18 w-col w-col-6" style="margin-left: -300px"><a class="button-14 w-button"
                                                 href="<?= "/~tmcvick/uploads/" . $rowData['filename'] ?>"
                                                 id="downloadBtn" download>Download</a>
         </div>
@@ -76,6 +76,7 @@ This audio failed to play
         </div>
     </div>
 </div>
+
 <div class="form-wrapper-4 w-form">
     <form class="form-7" data-name="Email Form 3" id="email-form-3" name="email-form-3" action="../php/create_comment.php" method="post">
         <h3 align="center" style="color:#ff5c00">Comments</h3>
